b1bd543d83ad1a92d7bae83c5a2b0376821919b6
Ilson <ilson@inobrega.com.br>
2015-05-12 22:45:49 -0300

    Iniciando com Definitions do Zend\Di 2

--- index.php
+++ index.php
@@ -11,19 +11,21 @@ $loader->registerNamespace('SON', 'library/SON');
 $loader->register();
 
 $definitionList = new Zend\Di\DefinitionList(array(
-    new Zend\Di\Definition\ArrayDefinition(include __DIR__ . '/data/di/SON-definition.php')
+    new Zend\Di\Definition\ArrayDefinition(include __DIR__ . '/data/di/SON-definition.php'),
+    $runtime = new Zend\Di\Definition\RuntimeDefinition()
 ));
 
 $di = new \Zend\Di\Di($definitionList);
-
 $di->instanceManager()->setParameters('SON\Db\Connection', array(
     'server' => 'localhost',
     'dbname' => 'banco',
     'user'  =>  'postgres',
     'password' => 123456
 ));
-$produto = $di->get('SON\Produto');
+$di->instanceManager()->addAlias('Produto', 'SON\Produto');
+$produto = $di->get('Produto');
 
+print_r($produto);exit;
 $categoria = $di->get('SON\Categoria');
 
 @Zend\Di\Display\Console::export($di);
